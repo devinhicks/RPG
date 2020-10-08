9f41e09d2266540a1b31fb471901418d68f624cb
Devin Hicks <devinhicks.az@gmail.com>
2020-10-08 00:00:00 -0700

    initial commit

---
+++ RPG/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public int maxPlayers = 10;
+
+    // instance
+    public static NetworkManager instance;
+
+    public void Awake()
+    {
+        if (instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // connect to the master server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We've connected to the master server!");
+        PhotonNetwork.JoinLobby();
+    }
+
+    // attempts to create a room
+    public void CreateRoom(string roomName)
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = (byte)maxPlayers;
+
+        PhotonNetwork.CreateRoom(roomName, options);
+    }
+
+    // attempts to join a room
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    [PunRPC]
+    public void ChangeScene(string sceneName)
+    {
+        Debug.Log("Change scene called");
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        PhotonNetwork.LoadLevel("Menu");
+    }
+
+    //punRPC??
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        //GameManager.instance.alivePlayers--;
+        //GameUI.instance.UpdatePlayerInfoText();
+
+        if(PhotonNetwork.IsMasterClient)
+        {
+            //GameManager.instance.CheckWinCondition();
+        }
+    }
+}
